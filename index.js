d630f908b2793d2507e6ba2e9daa36a63e212617
DepinKunn <88582191+DepinKunn@users.noreply.github.com>
2021-08-08 23:34:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ const { jadibot, stopjadibot, listjadibot } = require('./lib/jadibot')
 
 banChats = true
 offline = false
-targetpc = '6285751056816'
-owner = '6285751056816'
-fake = 'HEXAGONZ'
+targetpc = '19209314636'
+owner = '628871746203'
+fake = 'DEPINKUNN'
 numbernye = '0'
 waktu = '-'
 alasan = '-'
